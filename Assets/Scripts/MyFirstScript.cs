77d2381d6ef6ecdac0cbca63679d98c8255ec988
steve rector <steve.rector@datadoghq.com>
2022-09-16 15:17:17 -0700

    Adds ability to turn down volume with slider and updates total muffin count UI on cick

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MyFirstScript : MonoBehaviour
 {
     private int _counter = 0;
+
+    [SerializeField] private int _muffinsPerClick = 1;
+    [SerializeField] private TextMeshProUGUI _totalMuffinsText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +24,8 @@ public class MyFirstScript : MonoBehaviour
 
     public void OnMuffinClicked()
     {
-        _counter++;
+        _counter += _muffinsPerClick;
+        _totalMuffinsText.text = _counter.ToString();
         Debug.Log(_counter);
     }
 }
--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeController : MonoBehaviour
 {
+    [SerializeField] Slider _volumeSlider;
+
     public void OnVolumeChanged()
     {
+        AudioListener.volume = _volumeSlider.value;
         Debug.Log("volume slider changed");
     }
 }
