ea71fcaeefe3c223b31f594de4592c0616e73c0f
steve rector <steve.rector@datadoghq.com>
2022-09-17 15:28:23 -0700

    Animates spinlights behind muffin
    utilizes mathf.sin and manipulates the distance, speed and offset to make a pulsing looking animation

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -10,10 +10,27 @@ public class MyFirstScript : MonoBehaviour
     [SerializeField] private int _muffinsPerClick = 1;
     [Range(0, 100)][SerializeField] private int _criticalPercentChance = 1;
     [SerializeField] private TextMeshProUGUI _totalMuffinsText;
+    [SerializeField] private Transform _spinLight1, _spinLight2;
+    [SerializeField] private float _spinSpeed = 360f;
+    [SerializeField] private float _sinDistance = 1f;
+    [SerializeField] private float _sinSpeed = 1f;
+    [SerializeField] private float _waveOffset = 0f;
 
     void Start()
     {
         UpdateTotalMuffins();
+    }
+
+    private void Update()
+    {
+        Vector3 rotation = new Vector3();
+        rotation.z = _spinSpeed * Time.deltaTime;
+        _spinLight1.Rotate(rotation);
+        _spinLight2.Rotate(rotation);
+        PulseSpinLight(_spinLight1);
+        PulseSpinLight(_spinLight2);
+
+
     }
 
     public void OnMuffinClicked()
@@ -55,6 +72,12 @@ public class MyFirstScript : MonoBehaviour
         {
             return false;
         }
+    }
+
+    void PulseSpinLight(Transform spinLight)
+    {
+        float sin = (Mathf.Sin(Time.time * _sinSpeed) * _sinDistance) + _waveOffset;
+        spinLight.transform.localScale = new Vector3(sin, sin, 0f);
     }
 }
 
